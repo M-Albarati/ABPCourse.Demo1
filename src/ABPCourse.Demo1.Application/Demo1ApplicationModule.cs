8b0510a1c24011de78ead799343b140df7147982
albarati <mohammedbarati@gmail.com>
2026-01-12 21:13:22 +0300

    Caching In_Memory and DistributedCache in Redis

--- src/ABPCourse.Demo1.Application/Demo1ApplicationModule.cs
+++ src/ABPCourse.Demo1.Application/Demo1ApplicationModule.cs
@@ -1,5 +1,8 @@
-﻿using Volo.Abp.Account;
+﻿using System;
+using Volo.Abp.Account;
 using Volo.Abp.AutoMapper;
+using Volo.Abp.Caching;
+using Volo.Abp.Caching.StackExchangeRedis;
 using Volo.Abp.FeatureManagement;
 using Volo.Abp.FluentValidation;
 using Volo.Abp.Identity;
@@ -19,7 +22,8 @@ namespace ABPCourse.Demo1;
     typeof(AbpTenantManagementApplicationModule),
     typeof(AbpFeatureManagementApplicationModule),
     typeof(AbpSettingManagementApplicationModule),
-    typeof(AbpFluentValidationModule)
+    typeof(AbpFluentValidationModule),
+    typeof(AbpCachingStackExchangeRedisModule) // caching
     )]
 public class Demo1ApplicationModule : AbpModule
 {
@@ -28,6 +32,13 @@ public class Demo1ApplicationModule : AbpModule
         Configure<AbpAutoMapperOptions>(options =>
         {
             options.AddMaps<Demo1ApplicationModule>();
+        });
+
+        //redis caching
+        Configure<AbpDistributedCacheOptions>(options =>
+        {
+            options.KeyPrefix = "Demo1_";
+            options.GlobalCacheEntryOptions.AbsoluteExpiration = DateTimeOffset.Now.AddHours(1);
         });
     }
 }
---
+++ src/ABPCourse.Demo1.Application/Lookups/LookupsAppService.cs
@@ -0,0 +1,71 @@
+﻿using ABPCourse.Demo1.Bases;
+using ABPCourse.Demo1.Categories;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Caching;
+using Volo.Abp.Domain.Repositories;
+
+namespace ABPCourse.Demo1.Lookups
+{
+    public class LookupsAppService : BaseApplicationService
+    {
+        #region Fields
+        private readonly IRepository<Category, int> _categoryRepository;
+        private readonly IDistributedCache<List<CategoryDto>> _categoryCache;
+        #endregion
+
+        #region ctor
+        public LookupsAppService(IRepository<Category, int> categoryRepository,
+                                 IDistributedCache<List<CategoryDto>> categoryCache)
+        {
+            _categoryRepository = categoryRepository;
+            _categoryCache = categoryCache;
+        }
+        #endregion
+
+        #region Methods
+        public async Task<List<CategoryDto>?> GetCategories()
+        {
+            //return await GetAllCategoriesFromDbAsync();
+            //return await GetAllCategoriesFromCacheAsync();
+            return await GetAllCategoriesFromRedisCacheAsync();
+        }
+        #endregion
+
+        #region private Methods
+        
+        // get Data From DB
+        private async Task<List<CategoryDto>> GetAllCategoriesFromDbAsync()
+        {
+            var categories = await _categoryRepository.GetListAsync();
+            return ObjectMapper.Map<List<Category>, List<CategoryDto>>(categories);
+        }
+
+        // IN_Memory Caching
+        private async Task<List<CategoryDto>?> GetAllCategoriesFromCacheAsync()
+        {
+            return await _categoryCache.GetOrAddAsync(
+                $"ALL_CATEGORIES", //Cache Key
+                async () => await GetAllCategoriesFromDbAsync(), //Cache Value
+                () => new DistributedCacheEntryOptions //Cache Options
+                {
+                    AbsoluteExpiration = DateTimeOffset.Now.AddHours(1)
+                }
+            );
+        }
+
+        // Redis Caching
+        private async Task<List<CategoryDto>?> GetAllCategoriesFromRedisCacheAsync()
+        {
+            return await _categoryCache.GetOrAddAsync(
+                $"ALL_CATEGORIES", //Cache Key
+                async () => await GetAllCategoriesFromDbAsync() //Cache Value
+            );
+        }
+        #endregion
+    }
+}
